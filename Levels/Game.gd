4e5a99251f39dc599a42124292bb0c65b1b099b2
Dratonix <jas.mj@protonmail.com>
2024-12-29 19:55:17 -0600

    Imlemented split screen fully

--- Levels/Game.gd
+++ Levels/Game.gd
@@ -3,18 +3,21 @@ extends Node
 @export var viewport_size = Vector2i(960,540)
 @onready var players := {
 	"1": {
+		container = $HBoxContainer/SubViewportContainer,
 		viewport = $HBoxContainer/SubViewportContainer/ViewPort,
 		camera = $HBoxContainer/SubViewportContainer/ViewPort/Camera1,
 		player = $HBoxContainer/SubViewportContainer/ViewPort/Level1/Player
 	},
 	"2":
 		{
+		container = $HBoxContainer/SubViewportContainer2,
 		viewport = $HBoxContainer/SubViewportContainer2/ViewPort,
 		camera = $HBoxContainer/SubViewportContainer2/ViewPort/Camera2D,
 		player = $HBoxContainer/SubViewportContainer/ViewPort/Level1/Player2
 		},
 	"3":
 		{
+		container = $HBoxContainer/SubViewportContainer3,
 		viewport = $HBoxContainer/SubViewportContainer3/ViewPort,
 		camera = $HBoxContainer/SubViewportContainer3/ViewPort/Camera2D,
 		player = $HBoxContainer/SubViewportContainer/ViewPort/Level1/Player3
@@ -24,16 +27,23 @@ extends Node
 func _ready() -> void: 
 	viewport_size.x = viewport_size.x / Events.num_player
 	match  Events.num_player:
+		1:
+			print('f')
+			players["2"].container.queue_free()
+			players["3"].container.queue_free()
+			
 		2:
 			players["2"].viewport.world_2d = players["1"].viewport.world_2d
-			players["2"].viewport.size = viewport_size
-			players["1"].viewport.size = viewport_size
+			players["3"].container.queue_free()
+		3:
+			players["2"].viewport.world_2d = players["1"].viewport.world_2d
+			players["3"].viewport.world_2d  = players["1"].viewport.world_2d
+			
 	var i = 0
 	for node in players.values():
-		
+		node.viewport.size = viewport_size
 		var remote_transform = RemoteTransform2D.new()
 		remote_transform.remote_path = node.camera.get_path()
 		node.player.add_child(remote_transform)
-		print(node.player)
 		i+=1
-	print(viewport_size)
+		print(node.viewport.size)
